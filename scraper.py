b064503e69249e4543b4746ed4b1b2a889c8ca34
Talia <talia9tailz@pm.me>
2022-02-08 12:54:34 -0500

    Update scraper.py
    Optimized code

--- scraper.py
+++ scraper.py
@@ -1,31 +1,32 @@
 import requests, csv
 
 # obtain input spreadsheet
-input_sheet = requests.get('https://docs.google.com/spreadsheets/d/1H1a9eBamflt3w-4BPEk1kJYc4VgsDBWlDjkS0hV5tAY/export?format=csv')
+input_sheet = requests.get('https://docs.google.com/spreadsheets/d/1H1a9eBamflt3w-4BPEk1kJYc4VgsDBWlDjkS0hV5tAY/ex
+port?format=csv')
 f = open('input.csv', 'wb')
 f.write(input_sheet.content)
 f.close()
 # create output spreadsheet
-f = open('output.csv', 'w')
-f.write('')
-f.close()
-
-# open spreadsheet
+f_out = open('output.csv', 'w')
+f_out.write('')
+f_out.close()
+# open input sheet
 with open('input.csv', newline='') as f_in:
     reader = csv.reader(f_in)
-    # initialize iteration counter
-    n = 0
-    # iterate over rows in sheet
-    for row in reader:
+    # open output sheet in append mode
+    with open('output.csv', 'a', newline='') as f_out:
+        writer = csv.writer(f_out)
         # process header row
-        if n == 0:
-            row.append('Valid')
-        # process data rows
-        else:
+        header_row = next(reader)
+        header_row.append('Valid')
+        writer.writerow(header_row)
+        # prepare request
+        headers = { 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:96.0) Gecko/20100101 Firefox/96.0' }
+        url = 'https://tools.usps.com/tools/app/ziplookup/zipByAddress'
+        # iterate over rows in sheet
+        for row in reader:
             # format request
             data = { 'address1': row[1], 'zip': row[4] }
-            headers = { 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:96.0) Gecko/20100101 Firefox/96.0' }
-            url = 'https://tools.usps.com/tools/app/ziplookup/zipByAddress'
             # request address lookup through USPS
             r = requests.post(url=url, headers=headers, data=data)
             # determine address validity
@@ -33,9 +34,5 @@ with open('input.csv', newline='') as f_in:
                 row.append('False')
             else:
                 row.append('True')
-        # write to output sheet
-        with open('output.csv', 'a', newline='') as f_out:
-            writer = csv.writer(f_out)
+            # write to output sheet
             writer.writerow(row)
-        # increment iteration counter
-        n+=1
